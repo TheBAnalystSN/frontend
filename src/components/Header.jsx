0a57c401ebed410b9e6e4c21ee9c31cde9d6ecb0
[Dustin Robertson] <mrdustinrobertson@gmail.com>
2026-02-03 14:32:24 -0500

    feat(UI): added reusable Header component with title and subtitle

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>ITSM Portal</h1>
+      <span>IT Support Ticketing System</span>
+    </header>
+  );
+};
+
+export default Header;
